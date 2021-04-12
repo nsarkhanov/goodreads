c1891b8901e80338294706f59e18cd71de96e1ca
fistadev <fabiofistarol@gmail.com>
2021-04-12 22:58:57 +0200

    Update libraries.py

--- libraries.py
+++ libraries.py
@@ -1,10 +1,9 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from bs4 import BeautifulSoup, SoupStrainer
+from bs4 import BeautifulSoup
 import urllib.request
 import requests
 import re
 import csv
 import html
-import scrapy
