4ac3f32d8c4a78dc9235236182d5fcd6891bedc9
fistadev <fabiofistarol@gmail.com>
2021-04-12 17:34:23 +0200

    Create libraries.py

---
+++ libraries.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup, SoupStrainer
+import urllib.request
+import requests
+import re
+import csv
+import html
+import scrapy
